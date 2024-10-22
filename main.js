33159965fdcfbbe91777cbee66b7e6a3290669ad
JoubertColin <joubert.colin@yahoo.com.br>
2024-10-22 20:50:44 -0300

    Nearly finished pet card feature.

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ async function petsArea() {
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+
     wrapper.appendChild(clone)
   })
   document.querySelector(".list-of-pets").appendChild(wrapper)
@@ -27,3 +32,14 @@ async function petsArea() {
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a years old"
+
+  return `${age} years old`
+  //** ou ** age + " years old"
+}
