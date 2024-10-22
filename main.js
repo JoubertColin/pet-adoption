8dea436fce5ef078821c651319fa6e2efc5b0258
JoubertColin <joubert.colin@yahoo.com.br>
2024-10-22 19:49:28 -0300

    Work in progress, fetched pet data

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ async function start() {
 
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
-  console.log(weatherData.properties.periods[0].temperature)
 
   const ourTemperature = weatherData.properties.periods[0].temperature
   document.querySelector("#temperature-output").textContent = ourTemperature
@@ -10,3 +9,15 @@ async function start() {
 }
 
 start()
+
+async function petsArea() {
+
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.species)
+  })
+
+}
+
+petsArea()
